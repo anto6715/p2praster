a4619f4e92c5f8e4218000ba21daa1bc05e1b38d
Antonio <mariani.antonio@outlook.it>
2019-11-16 13:43:04 +0100

    update

--- error.cpp
+++ error.cpp
@@ -9,37 +9,74 @@
 #include "error.h"
 using namespace std;
 
-int fileError(string nameFunction) {
-    cerr << nameFunction << ": Can't open file: " << "clustered.csv" << endl;
-    return -7;
-}
-
-int readDatasetError(string nameFunction) {
-    cerr << nameFunction << ": Can't load dataset: " << "clustered.csv" << endl;
-    return -8;
-}
-
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -1
+ */
 int memoryError(string nameFunction) {
     cerr << nameFunction << ": Not enough memory" << endl;
     return -1;
 }
 
-int partitionError(string nameFunction) {
-    cerr << nameFunction << ": Error partition dataset" << endl;
-    return -9;
-}
-
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -2
+ */
 int insertError(string nameFunction) {
     cerr << nameFunction << ": Insert Error" << endl;
     return -2;
 }
 
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -3
+ */
+int dataError(string nameFunction) {
+    cerr << nameFunction << ": Corrupt data Error" << endl;
+    return -3;
+}
+
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -7
+ */
+int fileError(string nameFunction) {
+    cerr << nameFunction << ": Can't open file: " << "clustered.csv" << endl;
+    return -7;
+}
+
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -8
+ */
+int readDatasetError(string nameFunction) {
+    cerr << nameFunction << ": Can't load dataset: " << "clustered.csv" << endl;
+    return -8;
+}
+
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -9
+ */
+int partitionError(string nameFunction) {
+    cerr << nameFunction << ": Error partition dataset" << endl;
+    return -9;
+}
+
+/**
+ *
+ * @param [in] nameFunction - Name of the calling function
+ * @return -10
+ */
 int findError(string nameFunction) {
     cerr << nameFunction << ": Find Error" << endl;
     return -10;
 }
 
-int dataError(string nameFunction) {
-    cerr << nameFunction << ": Corrupt data Error" << endl;
-    return -3;
-}
+
